96fa1a4587f70702f15028dc5e84a977687a9903
Evgenii Neumerzhitckii <evgeny@bikeexchange.com.au>
2014-10-01 09:19:42 +1000

    Initial Commit

---
+++ swift-badge/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  swift-badge
+//
+//  Created by Evgenii Neumerzhitckii on 1/10/2014.
+//  Copyright (c) 2014 The Exchange Group Pty Ltd. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
