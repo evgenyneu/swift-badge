bcbb3b4d0e4635176a21cf83ca0f05d8945522a6
Evgenii Neumerzhitckii <evgeny@bikeexchange.com.au>
2014-10-01 17:18:46 +1000

    Use storyboard to position badge

--- swift-badge/SwiftBadge.swift
+++ swift-badge/SwiftBadge.swift
@@ -9,65 +9,67 @@
 import UIKit
 
 class SwiftBadge: UILabel {
-  var defaultInsets = CGSize(width: 6, height: 2)
+  var defaultInsets = CGSize(width: 2, height: 2)
   var actualInsets = CGSize()
-
+  
   override init() {
     super.init()
-
+    
     setup()
   }
-
+  
   override init(frame: CGRect) {
     super.init(frame: frame)
   }
-
+  
   required init(coder aDecoder: NSCoder) {
     super.init(coder: aDecoder)
+    
+    setup()
   }
-
+  
   private func setup() {
     setTranslatesAutoresizingMaskIntoConstraints(false)
-
+    
     layer.backgroundColor = UIColor.redColor().CGColor
     textColor = UIColor.whiteColor()
-
+    
     // Shadow
     layer.shadowOpacity = 0.5
     layer.shadowOffset = CGSize(width: 0, height: 0)
     layer.shadowRadius = 0.5
     layer.shadowColor = UIColor.blackColor().CGColor
   }
-
+  
   // Add custom insets
   // --------------------
   override func textRectForBounds(bounds: CGRect, limitedToNumberOfLines numberOfLines: Int) -> CGRect {
     let rect = super.textRectForBounds(bounds, limitedToNumberOfLines: numberOfLines)
-
+    
     actualInsets = defaultInsets
     var rectWithDefaultInsets = CGRectInset(rect, -actualInsets.width, -actualInsets.height)
-
+    
     // If width is less than height
     // Adjust the width insets to make it look round
     if rectWithDefaultInsets.width < rectWithDefaultInsets.height {
       actualInsets.width = (rectWithDefaultInsets.height - rect.width) / 2
     }
-
+    
     return CGRectInset(rect, -actualInsets.width, -actualInsets.height)
   }
-
+  
   override func drawTextInRect(rect: CGRect) {
-
+    
     layer.cornerRadius = rect.height / 2
-
+    
     let insets = UIEdgeInsets(
       top: actualInsets.height,
       left: actualInsets.width,
       bottom: actualInsets.height,
       right: actualInsets.width)
-
+    
     let rectWithoutInsets = UIEdgeInsetsInsetRect(rect, insets)
-
+    
     super.drawTextInRect(rectWithoutInsets)
   }
 }
--- swift-badge/ViewController.swift
+++ swift-badge/ViewController.swift
@@ -9,14 +9,13 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+  @IBOutlet weak var badge: SwiftBadge!
+  
   @IBOutlet weak var valueStepper: UIStepper!
   @IBOutlet weak var insetStepper: UIStepper!
 
   let defaultBadgeValue = 1.0
   let defaultInsetValue = 2.0
-
-  var badge: SwiftBadge?
 
   override func viewDidLoad() {
     super.viewDidLoad()
@@ -24,52 +23,17 @@ class ViewController: UIViewController {
     valueStepper.value = defaultBadgeValue
     insetStepper.value = defaultInsetValue
 
-    createBadge()
-
     updatebadgeValue()
     updateInsetValue()
   }
 
-  private func createBadge() {
-    let newBadge = SwiftBadge()
-
-    newBadge.defaultInsets = CGSize(width: 12, height: 12)
-    badge = newBadge
-    newBadge.font = UIFont.preferredFontForTextStyle(UIFontTextStyleBody)
-    view.addSubview(newBadge)
-
-    addCenterXConstraints(newBadge)
-    addCenterYConstraints(newBadge)
-  }
-
-  private func addCenterXConstraints(chilView: UIView) {
-    view.addConstraint(NSLayoutConstraint(item: view,
-      attribute: NSLayoutAttribute.CenterX,
-      relatedBy: NSLayoutRelation.Equal,
-      toItem:chilView,
-      attribute: NSLayoutAttribute.CenterX,
-      multiplier:1,
-      constant:0))
-  }
-
-  private func addCenterYConstraints(chilView: UIView) {
-    view.addConstraint(NSLayoutConstraint(item: view,
-      attribute: NSLayoutAttribute.CenterY,
-      relatedBy: NSLayoutRelation.Equal,
-      toItem:chilView,
-      attribute: NSLayoutAttribute.CenterY,
-      multiplier:1,
-      constant:0))
-  }
   
   @IBAction func onStepperChanged(stepper: UIStepper) {
     updatebadgeValue()
   }
 
   private func updatebadgeValue() {
-    if let currentBadge = badge {
-      currentBadge.text = formatBadgeValue(valueStepper.value)
-    }
+    badge.text = formatBadgeValue(valueStepper.value)
   }
 
   @IBAction func onInsetStepperChanged(sender: AnyObject) {
@@ -77,11 +41,9 @@ class ViewController: UIViewController {
   }
 
   private func updateInsetValue() {
-    if let currentBadge = badge {
-      var inset = insetStepper.value
-      currentBadge.defaultInsets = CGSize(width: inset, height: inset)
-      currentBadge.invalidateIntrinsicContentSize()
-    }
+    var inset = insetStepper.value
+    badge.defaultInsets = CGSize(width: inset, height: inset)
+    badge.invalidateIntrinsicContentSize()
   }
 
   private func formatBadgeValue(value: Double) -> String {
